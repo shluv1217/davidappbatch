78d5830d148da9f8a3835a7449f7bc67cbe3fc5e
shluv1217 <shluv1217@gmail.com>
2019-09-08 17:10:40 +0900

    initial commit

---
+++ davidappbatch/src/main/java/com/david/davidappbatch/DavidappbatchApplication.java
@@ -0,0 +1,13 @@
+package com.david.davidappbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavidappbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavidappbatchApplication.class, args);
+	}
+
+}
