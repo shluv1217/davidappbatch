544e6ffed57b8fc56d2bc09c20e416b609de9237
shluv1217 <shluv1217@gmail.com>
2019-09-09 10:22:57 +0900

    update

---
+++ davidappbatch/src/main/java/com/david/davidappbatch/PersonItemProcessor.java
@@ -0,0 +1,30 @@
+package com.david.davidappbatch;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class PersonItemProcessor implements ItemProcessor<Person, Person> {
+
+    private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
+
+    @Override
+    public Person process(final Person person) throws Exception {
+    	
+    	ClassLoader classLoader = getClass().getClassLoader();
+    	//File file = new File(classLoader.getResource("/images/jsonschema.json").getFile());
+    	
+    	
+        final String firstName = person.getFirstName().toUpperCase();
+        final String lastName = person.getLastName().toUpperCase();
+
+        final Person transformedPerson = new Person(firstName, lastName);
+
+        log.info("Converting (" + person + ") into (" + transformedPerson + ")");
+
+        return transformedPerson;
+    }
+}
